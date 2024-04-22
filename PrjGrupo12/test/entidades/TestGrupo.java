c80e4972103f878daecff97c6ffb586d8ab57e54
Nvkn <nicolasbarciaq@gmail.com>
2024-04-22 21:52:32 +0200

    TestGrupo añadido.

--- PrjGrupo12/test/entidades/TestGrupo.java
+++ PrjGrupo12/test/entidades/TestGrupo.java
@@ -2,14 +2,65 @@ package entidades;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestGrupo {
 
-	@Test
-	@DisplayName("CP2_001 ")
-	void testGrupo() {
-		fail("Not yet implemented");
-	}
+    @DisplayName("CP2_001: ID is null")
+    @Test
+    void testIdNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Grupo("asd", null, "asd");
+        });
+    }
+
+    @DisplayName("CP2_002: ID is empty")
+    @Test
+    void testIdEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Grupo("asd", "", "asd");
+        });
+    }
+
+    @DisplayName("CP2_003: Title is null")
+    @Test
+    void testTituloNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Grupo(null, "asd", "asd");
+        });
+    }
+
+    @DisplayName("CP2_004: Title is empty")
+    @Test
+    void testTituloEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Grupo("", "asd", "asd");
+        });
+    }
+
+    @DisplayName("CP2_005: Description is null")
+    @Test
+    void testDescripcionNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Grupo("asd", "asd", null);
+        });
+    }
+
+    @DisplayName("CP2_006: Description is empty")
+    @Test
+    void testDescripcionEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Grupo("asd", "asd", "");
+        });
+    }
+
+    @DisplayName("CP2_007: All inputs are valid")
+    @Test
+    void testValidInputs() {
+        assertDoesNotThrow(() -> {
+            new Grupo("asd", "asd", "asd");
+        });
+    }
 
 }
