7293a0de0105a969de6864834b30d406aaa15375
Guillermo Madrinan <55517487+Skioh@users.noreply.github.com>
2024-04-23 12:38:58 +0200

    Actualiza Displays y correccion pruebas

--- PrjGrupo12/test/entidades/TestGasto.java
+++ PrjGrupo12/test/entidades/TestGasto.java
@@ -2,10 +2,12 @@ package entidades;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestGasto {
 
+    @DisplayName("CP3_001: ID nulo")
     @Test
     public void testIdInvalidoNulo() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -13,27 +15,23 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_002: ID vacio")
     @Test
     public void testIdInvalidoVacio() {
         assertThrows(IllegalArgumentException.class, () -> {
             new Gasto("", 10, "Id_U_Ejemplo", "Id_G_Ejemplo");
         });
     }
-
+        
+    @DisplayName("CP3_003: Cantidad igual a 0")
     @Test
     public void testCantidadInvalidaCero() {
         assertThrows(IllegalArgumentException.class, () -> {
             new Gasto("asd", 0, "Id_U_Ejemplo", "Id_G_Ejemplo");
         });
     }
-
-    @Test
-    public void testCantidadInvalidaNegativa() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Gasto("asd", -1, "Id_U_Ejemplo", "Id_G_Ejemplo");
-        });
-    }
-
+    
+    @DisplayName("CP3_004: ID Usuario nulo")
     @Test
     public void testIdUsuarioInvalidoNulo() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -41,6 +39,7 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_005: ID Usuario vacío ")
     @Test
     public void testIdUsuarioInvalidoVacio() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -48,6 +47,7 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_006: ID Usuario con insuficientes caracteres (3) ")
     @Test
     public void testrIdUsuarioInvalidoCorto() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -55,6 +55,7 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_007: ID Usuario con demasiados caracteres (21) ")
     @Test
     public void testIdUsuarioInvalidoLargo() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -62,6 +63,7 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_008: ID Grupo nulo ")
     @Test
     public void testIdGrupoGastoInvalidoNulo() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -69,6 +71,7 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_009: ID Grupo vacío")
     @Test
     public void testIdGrupoGastoInvalidoVacio() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -76,6 +79,7 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_010: ID Grupo con insuficientes caracteres (3)")
     @Test
     public void testIdGrupoGastoInvalidoCorto() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -83,6 +87,7 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_011: ID Grupo con demasiados caracteres (21)")
     @Test
     public void testIdGrupoGastoInvalidoLargo() {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -90,19 +95,22 @@ class TestGasto {
         });
     }
 
+    @DisplayName("CP3_012: IDs con el mínimo número de caracteres permitido")
     // Pruebas para casos válidos
     @Test
     public void testValidoAvlIdUsuarioGrupo1() {
         assertDoesNotThrow(() -> {
-            new Gasto("asd", 10, "ID_U", "Id_G");
+            new Gasto("asd", 0.01, "ID_U", "Id_G");
         });
     }
     
+    @DisplayName("CP3_013: IDs con el máximo número de caracteres permitido")
     @Test
     public void testValidoAvlIdUsuarioGrupo2() {
         assertDoesNotThrow(() -> {
-            new Gasto("asd", 10, "idde20caracteresssss", "ID_G5678901234567890");
+            new Gasto("asd", 0.01, "idde20caracteresssss", "ID_G5678901234567890");
         });
     }
 
 }
+
