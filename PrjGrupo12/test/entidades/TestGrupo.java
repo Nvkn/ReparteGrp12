94bbc6fa5529f1123ee68aec6779661574d0fec5
Nvkn <nicolasbarciaq@gmail.com>
2024-04-29 21:20:52 +0200

    Test Grupo finalizados

--- PrjGrupo12/test/entidades/TestGrupo.java
+++ PrjGrupo12/test/entidades/TestGrupo.java
@@ -2,93 +2,86 @@ package entidades;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class TestGrupo {
+    static Usuario usuarioValido;
 
-	@Nested
-	class RegistrarGrupo {
-	    @DisplayName("CP2_001: ID null")
-	    @Test
-	    void testIdNull() {
-	        assertThrows(IllegalArgumentException.class, () -> {
-	            new Grupo("Título", null, "Descripción");
-	        });
-	    }
-	
-	    @DisplayName("CP2_002: ID vacío")
-	    @Test
-	    void testIdVacio() {
-	        assertThrows(IllegalArgumentException.class, () -> {
-	            new Grupo("Título", "", "Descripción");
-	        });
-	    }
-	
-	    @DisplayName("CP2_003: ID corto")
-	    @Test
-	    void testIdIdCorto() {
-	        assertThrows(IllegalArgumentException.class, () -> {
-	            new Grupo("Título", "IDG", "Descripción");
-	        });
-	    }
-	
-	    @DisplayName("CP2_004: ID largo")
-	    @Test
-	    void testIdIdLargo() {
-	        assertThrows(IllegalArgumentException.class, () -> {
-	            new Grupo("Título", "ID_G56789012345678901", "Descripción");
-	        });
-	    }
-	
-	    @DisplayName("CP2_005: Título null")
-	    @Test
-	    void testTituloNull() {
-	        assertThrows(IllegalArgumentException.class, () -> {
-	            new Grupo(null, "ID_G", "Descripción");
-	        });
-	    }
-	
-	    @DisplayName("CP2_006: Título vacío")
-	    @Test
-	    void testTituloVacio() {
-	        assertThrows(IllegalArgumentException.class, () -> {
-	            new Grupo("", "ID_G", "Descripción");
-	        });
-	    }
-	
-	    @DisplayName("CP2_007: Descripción null")
-	    @Test
-	    void testDescripcionNull() {
-	        assertThrows(IllegalArgumentException.class, () -> {
-	            new Grupo("Título", "ID_G", null);
-	        });
-	    }
-	
-	    @DisplayName("CP2_008: Descripción vacía")
-	    @Test
-	    void testDescripcionVacia() {
-	        assertThrows(IllegalArgumentException.class, () -> {
-	            new Grupo("Título", "ID_G", "");
-	        });
-	    }
-	
-	    @DisplayName("CP2_009: Todo válido - ID Min")
-	    @Test
-	    void testValidoIdMin() {
-	        assertDoesNotThrow(() -> {
-	            new Grupo("Título", "ID_G", "Descripción");
-	        });
-	    }
-	
-	    @DisplayName("CP2_010: Todo válido - ID Max")
-	    @Test
-	    void testValidoIdMax() {
-	        assertDoesNotThrow(() -> {
-	            new Grupo("Título", "ID_G5678901234567890", "Descripción");
-	        });
-	    }
-	}
+    @BeforeAll
+    static void setUp() {
+        usuarioValido = new Usuario("ID1234", "correo@ejemplo.com", "Abc123..");
+    }
+
+    @Nested
+    @DisplayName("CP_GR1: Casos de prueba del constructor")
+    class ConstructorTest {
+        @Nested
+        @DisplayName("Clases de equivalencia no válidas de: Título")
+        class CPTitulo {
+            @DisplayName("CP_GR1_01: Título nulo")
+            @Test
+            void CP_GR1_01() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                    new Grupo(null, "Descripcion", usuarioValido);
+                });
+            }
+
+            @DisplayName("CP_GR1_02: Título vacío")
+            @Test
+            void CP_GR1_02() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                    new Grupo("", "Descripcion", usuarioValido);
+                });
+            }
+        }
+
+        @Nested
+        @DisplayName("Clases de equivalencia no válidas de: Descripcion")
+        class CPDescripcion {
+            @DisplayName("CP_GR1_03: Descripción nula")
+            @Test
+            void CP_GR1_03() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                    new Grupo("Título", null, usuarioValido);
+                });
+            }
+
+            @DisplayName("CP_GR1_04: Descripción vacía")
+            @Test
+            void CP_GR1_04() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                    new Grupo("Título", "", usuarioValido);
+                });
+            }
+        }
+
+        @Nested
+        @DisplayName("Clases de equivalencia no válidas de: Admin")
+        class CPAdmin {
+            @DisplayName("CP_GR1_05: Admin null")
+            @Test
+            void CP_GR1_05() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                    new Grupo("Título", "Descripción", null);
+                });
+            }
+        }
+
+        @Nested
+        @DisplayName("Clases de equivalencia válidas")
+        class CPValidas {
+            @DisplayName("CP_GR1_06: Todo válido - ID y Password Min ")
+            @Test
+            void CP_GR1_06() {
+                assertDoesNotThrow(() -> {
+                    new Grupo("Título", "Descripción", usuarioValido);
+                });
+            }
+        }
+    }
 
 }
+
